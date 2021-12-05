b66c12528f38718da1b8ee9f8681774e574d9b56
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2021-12-04 17:14:35 -0800

    matplotlib version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'gym[atari,box2d,classic_control]~=0.15.3',
         'ipython',
         'joblib',
-        'matplotlib==3.1.1',
+        'matplotlib>=3.1.1<4.0',
         'mpi4py',
         'numpy',
         'pandas',
